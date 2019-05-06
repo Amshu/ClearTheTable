32c81f0282d6d15264191c8cc6ef54ac96c65e42
Amshu <amshumaanrakshith@gmail.com>
2019-05-06 19:14:38 +1000

    Changed Asset Bundling Script & Added 2 of Bones Card
    - Added another function which compiles to a Windows build
    - Added a 2 of bones card to the test
    - Changed the Asset Bundle options of the folders to use the 2 of bones card

--- ClearTheTable/Assets/ThirdParty/CardFramework/Editor/AssetBundleBuild.cs
+++ ClearTheTable/Assets/ThirdParty/CardFramework/Editor/AssetBundleBuild.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 public class AssetBundleBuild
 {
 
-	[MenuItem("Assets/Build All Bundles OSX")]	
-	static public void BuildAllBundlesOSX() 
+	[MenuItem("Assets/Build All Bundles Windows")]	
+	static public void BuildAllBundlesWindows() 
 	{
 		BuildAssetBundleOptions assetBundleOptions = BuildAssetBundleOptions.UncompressedAssetBundle;
-		BuildPipeline.BuildAssetBundles(DirectoryUtility.ExternalAssets(), assetBundleOptions, BuildTarget.StandaloneOSXIntel);
+		BuildPipeline.BuildAssetBundles(DirectoryUtility.ExternalAssets(), assetBundleOptions, BuildTarget.StandaloneWindows);
 	}
-	
+
+    [MenuItem("Assets/Build All Bundles OSX")]
+    static public void BuildAllBundlesOSX()
+    {
+        BuildAssetBundleOptions assetBundleOptions = BuildAssetBundleOptions.UncompressedAssetBundle;
+        BuildPipeline.BuildAssetBundles(DirectoryUtility.ExternalAssets(), assetBundleOptions, BuildTarget.StandaloneOSX);
+    }
+
 }
